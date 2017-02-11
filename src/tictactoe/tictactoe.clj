754c3990396ee7778c0a0cbe2ea69be940613ba6
adam <adam>
2017-02-11 11:59:16 +0000

    Fixed formatting

--- src/tictactoe/tictactoe.clj
+++ src/tictactoe/tictactoe.clj
@@ -16,22 +16,16 @@
                "  ---------------\n"
            "3 " (get-display-char (nth game-state 6)) " | " 
                (get-display-char (nth game-state 7)) " | " 
-               (get-display-char (nth game-state 8)) "\n")
-
-)
+               (get-display-char (nth game-state 8)) "\n"))
 
 (defn check-input-valid [input-string]
-  (not= (re-matches #"[A-C]{1}[1-3]{1}" input-string) nil)
-)
+  (not= (re-matches #"[A-C]{1}[1-3]{1}" input-string) nil))
 
 (defn parse-input [input-string]
   (if (check-input-valid input-string)
     (+
       (* (- (int (get input-string 1)) 49) 3)
-      (- (int (get input-string 0)) 65)
-    )
-  )    
-)
+      (- (int (get input-string 0)) 65))))
 
 (defn equality-excluding-nil [arg1 arg2 arg3]
   (and 
@@ -46,8 +40,7 @@
       (equality-excluding-nil (nth board 1) (nth board 4) (nth board 7))
       (equality-excluding-nil (nth board 2) (nth board 5) (nth board 8))
       (equality-excluding-nil (nth board 0) (nth board 4) (nth board 8))
-      (equality-excluding-nil (nth board 2) (nth board 4) (nth board 6)))
-)
+      (equality-excluding-nil (nth board 2) (nth board 4) (nth board 6))))
 
 (defn is-move-invalid [board move]
   (def move-as-number (parse-input move))
